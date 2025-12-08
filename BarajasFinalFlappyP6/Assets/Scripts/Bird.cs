4b36c14669ee557bfc2ad32bf82f0f63cfa12338
754972-glitch <754972@student.dusd.net>
2025-12-08 13:58:53 -0800

    added player character background
    started adding scripts

---
+++ BarajasFinalFlappyP6/Assets/Scripts/Bird.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+private bool isDead = false;
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() 
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+       if (isDead == false)
+    }
+}
