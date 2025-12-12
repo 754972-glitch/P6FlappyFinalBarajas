713b68ad527b83aa37266eef42b765a011a8f154
754972-glitch <754972@student.dusd.net>
2025-12-12 13:40:12 -0800

    modified the code

--- BarajasFinalFlappyP6/Assets/Scripts/Bird.cs
+++ BarajasFinalFlappyP6/Assets/Scripts/Bird.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine; 
 
-public class NewBehaviourScript : MonoBehaviour
+public class Bird: MonoBehaviour
 {
     public float upForce = 200f;
     
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
     // Start is called before the first frame update
     void Start() 
     {
         rb2d = GetComponent<Rigidbody2D> ();
-           
+        anim = GetComponent<Animator>();  
     }
 
     // Update is called once per frame
@@ -25,7 +26,9 @@ public class NewBehaviourScript : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
             }
+
 
         }
 
@@ -33,6 +36,7 @@ public class NewBehaviourScript : MonoBehaviour
     private void OnCollisionEnter2D()
     {
         isDead = true;
-    }
+        anim.SetTrigger("Die"); 
+    }  
 
 }
